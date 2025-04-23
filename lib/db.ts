57afe4d87960a497803be149c36d43910fa587e7
Sneha Choudhary <snehachoudhary@Snehas-MacBook-Air.local>
2025-04-23 21:48:43 +0530

    Updated db connection to use environment variable for NeonDB

--- lib/db.ts
+++ lib/db.ts
@@ -1,8 +1,7 @@
 import { neon } from "@neondatabase/serverless"
 
 // Database connection
-const connectionString =
-  "postgresql://neondb_owner:npg_2jZuhHBn8FVm@ep-purple-surf-a4yofnt7-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require"
+const connectionString = process.env.NEON_DB_URL;
 
 // Create a SQL query function using the Neon serverless driver
 const sql = neon(connectionString)
